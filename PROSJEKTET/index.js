8cf6abde44400ab9d9b1031b07450157989c17dc
akselessum <AkselHenrikLessum@outlook.com>
2018-11-16 13:10:39 +0100

    Har fungerende kode som teller poeng og endrer spm, nå er det bare css og det p bytte ut mediene.

--- PROSJEKTET/index.js
+++ PROSJEKTET/index.js
@@ -1,25 +1,44 @@
-let all_questions = [{
-    question_string: "What color is the sky?",
-    choices: {
-        correct: "Blue",
-        wrong: ["Pink", "Orange", "Green"]
+
+let quiz = [
+    [0, "Hvilken artist gjemmer seg bak Danny sitt fjes?", "KeyA"],
+    [1, "EKSEMPELTEKST", "KeyB"],
+    [4, "EKSEMPELTEKST", "KeyC"],
+    [5, "EKSEMPELTEKST"],
+    [6, "EKSEMPELTEKST"],
+    [7, "EKSEMPELTEKST"],
+    [8, "EKSEMPELTEKST"],
+    [9, "EKSEMPELTEKST"],
+    [10, "EKSEMPELTEKST"],
+    [11, "EKSEMPELTEKST"],
+    [12, "EKSEMPELTEKST"],
+
+];
+
+let i = 0;
+let score = 0;
+let spm = document.getElementById("spm");
+spm.innerHTML = quiz[i][1];
+window.onkeydown = function (event) {
+    if (event.code === quiz[i][2]){
+        score = score + 1;
     }
-}, {
-    question_string: "Which of the following elements aren’t introduced in HTML5?",
-    choices: {
-        correct: "<input>",
-        wrong: ["<article>", "<footer>", "<hgroup>"]
+    i++;
+    spm.innerHTML = quiz[i][1];
+};
+window.onkeydown = function (event) {
+    if (event.code === quiz[i][2]){
+        score = score + 1;
     }
-}, {
-    question_string: "How many wheels are there on a tricycle?",
-    choices: {
-        correct: "Three",
-        wrong: ["One", "Two", "Four"]
+    i++;
+    spm.innerHTML = quiz[i][1];
+};
+window.onkeydown = function (event) {
+    if (event.code === quiz[i][2]){
+        score = score + 1;
     }
-}, {
-    question_string: 'Who is the main character of Harry Potter?',
-    choices: {
-        correct: "Harry Potter",
-        wrong: ["Hermione Granger", "Ron Weasley", "Voldemort"]
-    }
-}];
+    i++;
+    spm.innerHTML = quiz[i][1];
+};
+
+
+
