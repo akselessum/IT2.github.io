0236a5bafaef48098248574dadd3e2ea53b2b666
akselessum <AkselHenrikLessum@outlook.com>
2019-03-22 12:45:03 +0100

    IT2

--- EtterJul/SPILLPROSJEKT --------------------/faktiske prosjektet/sweeper.js
+++ EtterJul/SPILLPROSJEKT --------------------/faktiske prosjektet/sweeper.js
@@ -8,10 +8,9 @@ const ctx  = canvas.getContext("2d");
 let rader;
 let rekker;
 let tileArray = [];
-
 let tile = {
-  bredde: 50,
-  hoyde: 50,
+  bredde: 30,
+  hoyde: 30,
   farge: "green",
   bombe: "False"
 };
@@ -26,41 +25,42 @@ function startFunc(){
     tileArray = []; //Passer på at man wiper hele arrayet hver gang man startet på nytt
 
     if (vanskelighet.value === "Lav"){
-        canvas.height = 500;
-        canvas.width = 500;
+        canvas.height = 300;
+        canvas.width = 300;
     }
     else if (vanskelighet.value === "Middels"){
-        canvas.height = 500;
-        canvas.width = 750;
+        canvas.height = 480;
+        canvas.width = 480;
     }
     else if (vanskelighet.value === "Høy"){
-        canvas.height = 500 ;
-        canvas.width = 1000;
-    } //GJØR DE KVADRATISKE SÅ FUNKER DE SOM DE SKAL MED GRØNNFARGEN
+        canvas.height = 720 ;
+        canvas.width = 720;
+    }
 
     rader = canvas.width/tile.bredde;
     rekker = canvas.height/tile.hoyde;
 
+
     for(let i = 0; i < rekker; i++){
         tileArray.push([]);
-        let ypos = 50 * i;
+        let ypos = 30 * i;
 
         for (let h = 0; h < rader; h++){
 
             let bombe;
             let bombeSjanse = Math.random();
 
-            if(bombeSjanse > 0.8){
+            if(bombeSjanse > 0.8){//De to forige variablene og denne if'en randomizer en sjanse for at tilen som produseres har en bombe i seg, 20% sjanse.
                 bombe = true;
             } else {
                 bombe = false;
             }
 
-            let xpos = 50 * h;
+            let xpos = 30 * h;
 
             let tile = {
-                bredde: 50,
-                hoyde: 50,
+                bredde: 30,
+                hoyde: 30,
                 farge: "green",
                 bombe: bombe,
                 xpos: xpos,
@@ -80,16 +80,68 @@ function velgTile(event){ //Disse to for loopene sjekker om den tilen du trykker
         for (let tilen of tileArray[i]){
 
             if (event.layerX > tilen.xpos && event.layerX < tilen.xpos + tile.bredde
-                && event.layerY > tilen.ypos && event.layerY < tilen.ypos + tile.hoyde){
+                && event.layerY > tilen.ypos && event.layerY < tilen.ypos + tile.hoyde){ //Sjekker om trykket er innenfor tilen
 
-                if (tilen.bombe === false){
+                if (tilen.bombe === false){//Hva som skjer hvis hva du trykket ikke er en bombe
+
+                    let tileIndex = tileArray[i].indexOf(tilen);
+                    let grenseBomber = 0;
+
+                    if (i !== 0){
+                        if (tileArray[i-1][tileIndex].bombe === true){//Sjekker om tilen over er en bombe
+                            grenseBomber++;
+                        }
+                    }
+                    if (i !== 0 && tileIndex !== rader-1) {
+                        if (tileArray[i-1][tileIndex+1].bombe === true){//Sjekker om tilen diagonalt opp til høyre er en bomba
+                            grenseBomber++;
+                        }
+                    }
+                    if (i !== 0 && tileIndex !== 0){
+                        if (tileArray[i-1][tileIndex-1].bombe === true){//Sjekker om diagonalt opp til venstre er en bombe
+                            grenseBomber++;
+                        }
+                    }
+                    if (tileIndex !== 0){
+                        if (tileArray[i][tileIndex-1].bombe === true){//Sjekker om tilen til venstre er en bombe
+                            grenseBomber++;
+                        }
+                    }
+                    if (i !== rekker-1 && tileIndex !== 0){
+                        if (tileArray[i+1][tileIndex-1].bombe === true){//Sjekker om tilen diagonalt ned til venstre er en bombe
+                            grenseBomber++;
+                        }
+                    }
+                    if (tileIndex !== rader-1){
+                        if (tileArray[i][tileIndex+1].bombe === true){//Sjekker om tilen til høyre er en bombe
+                            grenseBomber++;
+                        }
+                    }
+                    if (i !== rekker-1){
+                        if (tileArray[i+1][tileIndex].bombe === true){//Sjekker om tilen under er en bombe
+                            grenseBomber++;
+                        }
+                    }
+                    if (i !== rekker-1 && tileIndex !== rader-1){
+                        if (tileArray[i+1][tileIndex+1].bombe === true){//Sjekker om tilen diagonalt ned til høyre er en bombe
+                            grenseBomber++;
+                        }
+                    }
 
                     tilen.farge = "blue ";
                     ctx.fillStyle = tilen.farge;
                     ctx.fillRect(tilen.xpos, tilen.ypos, tile.bredde-1, tile.hoyde-1);
 
+                    ctx.font = "30px Arial";
+                    ctx.fillStyle = "purple";
+                    ctx.fillText(grenseBomber, tilen.xpos+5, tilen.ypos+25);
+
+
                 } else {
-                    alert("pang effekt");
+                    tilen.farge = "red";
+                    ctx.fillStyle = tilen.farge;
+                    ctx.fillRect(tilen.xpos, tilen.ypos, tile.bredde-1, tile.hoyde-1);
+                    alert("du tapte nek");
                 }
 
             }
